705fcc08c9c745b0d8a1e351e001846d2dbd560f
Arnaud Debray <debray.arnaud@gmail.com>
2014-10-04 23:03:11 +0200

    Materials + Doku

--- Lua_client/main.lua
+++ Lua_client/main.lua
@@ -4,25 +4,10 @@ function love.load()
 	print('Lua_client')
 	client = require 'client':init()
 	-- pretty.dump(client)
-	tempo = 0
 end
 
 function love.update(dt)
-	tempo = tempo + dt
-
-	-- if tonumber(string.format("%.1f", tempo)) % 1 == 0 then
-		client:askInventory()
-		client:send("prend nourriture\n")
-		client:readline()
-		if client.str == 'ok' then
-			client.inventory.nourriture = client.inventory.nourriture + 1
-			print('got food !')
-		else
-			print('No OFOOD !')
-		end
-	-- end
-	client:look()
-	client:update()
+	client:update(dt)
 end
 
 function love.draw()
