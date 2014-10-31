fc728375d08cad0885a6ce47a2c35818e547e730
Arnaud Debray <debray.arnaud@gmail.com>
2014-11-01 00:31:44 +0100

    save home

--- serv/inc/serveur.h
+++ serv/inc/serveur.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   serveur.h                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: Arno <Arno@student.42.fr>                  +#+  +:+       +#+        */
+/*   By: adebray <adebray@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2014/10/02 16:15:34 by adebray           #+#    #+#             */
-/*   Updated: 2014/10/31 12:22:17 by Arno             ###   ########.fr       */
+/*   Updated: 2014/10/31 23:45:07 by adebray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,6 +26,11 @@ typedef struct	s_env
 	int			style;
 	int			protocol;
 }				t_env;
+
+typedef struct	s_client
+{
+
+}				t_client;
 
 void	ft_print_env(t_env *env);
 void	ft_fill_env(t_env *env, char ch, char *optarg);
